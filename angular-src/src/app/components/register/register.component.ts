e4c4ed0349b5350e1dc2c44c81e7db2ba64e1c0c
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2017-05-29 17:45:22 +0200

    Problem co;piling angular app

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,9 @@ export class RegisterComponent implements OnInit {
   password: String;
 
 constructor(private validateService: ValidateService,
-            private flashMessage: FlashMessagesService) { }
+           private flashMessage: FlashMessagesService,
+           private authService: AuthService,
+           private router: Router) { }
 
   ngOnInit() {
   }
@@ -41,7 +45,18 @@ constructor(private validateService: ValidateService,
       return false;
     }
 
-    console.log('success')
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success) {
+        this.flashMessage.show('You are now registered and can log in',
+                               {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('Something went wrong',
+                               {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
   }
 
 }
